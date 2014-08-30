66d2c407f05ae821e00db81ad04acf9c44806378
sarath <sarath.hacks@gmail.com>
2014-08-30 16:55:01 +0530

    Added dynamic script loding, Arranged Folder stucture

---
+++ jsc.bat
@@ -0,0 +1,8 @@
+:: Created by sarath
+:: August 22 2014
+:: Closure compiler for js minification
+echo off
+cls
+title Closure Compiler
+echo "[Closure compiler for js minification]"
+java -jar closure\compiler.jar %*
---
+++ src/main.js
@@ -0,0 +1,61 @@
+require.config({
+	shim:{
+		'collapse': {
+			deps: [ 'jquery','transition'],
+			exports: 'jQuery'
+		},
+		'transition':{
+			deps:['jquery'],
+			exports:'jQuery'
+		},
+		// Plax Plugin 
+		'plax': {
+			deps: [ 'jquery'],
+			exports: 'jQuery.fn.plaxify'
+		}
+		//End plax plugin shim
+		,
+		'backstretch':{
+			deps:['jquery'],
+			exports:'jQuery.fn.backstretch'
+		},
+		'fittext':{
+			deps:['jquery'],
+			exports:'jQuery.fn.fittext'
+		},
+		'nprogress':{
+			deps:['jquery'],
+			exports:'jQuery.fn.nprogress'
+		}
+		
+	},
+	paths:{
+		jquery:'js/jquery',
+		collapse:'js/collapse',
+		plax:'js/vendor/plax',
+		transition:'js/transition',
+		backstretch:'js/vendor/jquery.backstretch',
+		fittext:'js/vendor/jquery.fittext',
+		nprogress:'js/vendor/nprogress'
+	}
+});
+require(['jquery','collapse','backstretch','plax','fittext','nprogress'],function($,col,back,plax,fit,nprogress){
+	nprogress.start();
+	//alert('Whoops');
+
+	$(document).ready(function(){
+	//Document ready state
+
+		
+	$.backstretch('images/blur_green.jpg');
+	$('.fa').plaxify();
+	$.plax.enable();
+	
+	//Fittext Plugin
+	$(".rs-h1").fitText(1.2,{ minFontSize: '20px', maxFontSize: '40px' });
+	$(".rs-h3").fitText(2.2,{ minFontSize: '20px', maxFontSize: '32px'});
+	//Back Struch Plugin
+	nprogress.done();
+	//End Document Ready
+	});
+});
