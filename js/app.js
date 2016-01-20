bd17b07ab426a8baba95623b07885eac7982e3b2
Sarath <sarath.hacks@gmail.com>
2016-01-20 13:22:33 +0530

    App.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
-console.log("app");
+(function(){
+	console.log('Hai ');
 
+	// Creating components
+	var Contact = Vue.extend({
+    template: '<p>This is c!ontact page</p>'
+	});
+
+	// Creating router
+	var router = new VueRouter();
+
+	router.map({
+    '/contact': {
+        component: Contact
+    },
+    '/home': {
+        component: Contact
+    }
+	});
+
+	// Creating app
+	var App = Vue.extend({});
+
+
+	router.start(App,"#app");
+})();
