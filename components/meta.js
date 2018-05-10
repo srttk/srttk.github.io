d07c69ac5da716f33dcc876b7986e018737674ee
Sarath <saratonite@gmx.com>
2018-05-10 16:59:24 +0530

    :smile: sassssy

--- components/meta.js
+++ components/meta.js
@@ -1,45 +1,12 @@
 import Head from 'next/head'
-import { YELLOW } from '../config/colors'
+import { YELLOW } from '../config/colors';
+import '../styles/index.scss';
 export default ({ title }) => (
-    <div>
     <Head>
       <title>{ title }</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500" rel="stylesheet"/>
+      <link rel="stylesheet" href="/_next/static/style.css" />
     </Head>
-    <style jsx global>{`
-     *{
-          padding:0;
-          margin:0;
-          box-sizzing: border-box;
-      }
-      html {
-        height: 100%;
-      }
-
-      
-      body { 
-        font: 18px menlo;
-        font-family: "Montserrat", "Helvetica Neue", Arial, sans-serif;
-        height:100%;
-      }
-      ::-moz-selection { /* Code for Firefox */
-          background: yellow;
-      }
-      
-      ::selection {
-          background: ${YELLOW};
-      }
-      .app {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        color: #fff;
-        background: #2e2e4e;
-        min-height: 100vh;
-      }
-    `}</style>
-  </div>
 )
