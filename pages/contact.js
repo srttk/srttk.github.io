51ff45db44bda0ea1c1a401caf763d81847e0390
Sarath <sarath.hacks@gmail.com>
2018-05-13 22:54:26 +0530

    :zap: asdfg

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Screen from '../components/screen'
+import Header from '../components/header'
+import Meta from '../components/meta'
+
+export default () => (
+    <div>
+        <Meta title="Contact Sarath" />
+        <Screen>
+            <Header/>
+        </Screen>
+    </div>
+)
