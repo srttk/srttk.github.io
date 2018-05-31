c7cd0775e408bab644b25b2e6df6780c21910a41
Sarath <saratonite@gmx.com>
2018-05-31 17:47:25 +0530

    :bug: google analytics script issue fixed

--- components/meta.js
+++ components/meta.js
@@ -12,12 +12,7 @@ export default ({ title }) => (
      
       {/*  <!-- Global site tag (gtag.js) - Google Analytics --> */}
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-36053418-5"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-36053418-5');
-      </script>
+      <script dangerouslySetInnerHTML={{__html:`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-36053418-5');`}}/>
+      
     </Head>
 )
