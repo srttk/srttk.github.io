b45e554de70e6fa8d83140d838c093a7b170c2fe
Sarath <saratonite@gmx.com>
2018-06-20 13:02:10 +0530

    :hamburger: Export production flag

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withSass = require('@zeit/next-sass');
+const githubAssetUrl = "https://raw.githubusercontent.com/saratonite/saratonite.github.io/30ffd095/";
+
+const isProduction = true;
+
 module.exports = withSass({
     exportPathMap: function() {
 
         return {
             '/': { page: '/' }
         }
-    }
+    },
+    assetPrefix: isProduction ?  '' : githubAssetUrl
 });
