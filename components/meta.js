d7c2c7211bdb1c50830068dc1e60e9d70af0c085
Sarath <saratonite@gmx.com>
2018-06-20 18:33:01 +0530

    :bug: mobile view tweaks

--- components/meta.js
+++ components/meta.js
@@ -4,8 +4,8 @@ import '../styles/index.scss';
 export default ({ title }) => (
     <Head>
       <title>{ title }</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
       <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500" rel="stylesheet"/>
       <link rel="stylesheet" href="/_next/static/style.css" />
 
