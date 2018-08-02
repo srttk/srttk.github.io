bf665ba2c7e71422346154fda9d12dd8362b5781
Sarath <saratonite@gmx.com>
2018-08-02 10:37:30 +0530

    :boom: Package upgrades

--- components/Logo.js
+++ components/Logo.js
@@ -7,7 +7,7 @@ export default () => (
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
             version="1.1"
-            viewBox="0 0 180 200"
+            viewBox="0 0 180 160"
 
         >
         
